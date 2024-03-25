9fb4ed19c0a03ddadcd4d6e462a779415fa300bc
Arnoldas <arnoldvasiliauskas@gmail.com>
2024-03-25 20:23:25 +0200

    cfg korekcija

--- src/cfg/cfg.js
+++ src/cfg/cfg.js
@@ -2,7 +2,7 @@ export const cfg = {
   API: {
     HOST:
       process.env.NODE_ENV === 'production'
-        ? 'https://api-shop-arnoldas.vercel.app/product'
+        ? 'https://api-shop-arnoldas.vercel.app'
         : 'http://localhost:3000',
   },
 };
